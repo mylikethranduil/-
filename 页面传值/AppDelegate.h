35e3f862d7b969123714b13eab7737568ae12e88
bever <bever@beverdeimac.local>
2016-02-17 14:14:15 +0800

    Initial Commit

---
+++ 页面传值/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  页面传值
+//
+//  Created by bever on 16/2/17.
+//  Copyright © 2016年 bever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 页面传值/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  页面传值
+//
+//  Created by bever on 16/2/17.
+//  Copyright © 2016年 bever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
